667594cea1d7d29b619c0ba3f0c75fee7f6e527a
PaolaG365 <paola.georgieva.365@gmail.com>
2024-09-27 17:56:33 +0300

    urls added

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register_acc(request):
+    return render(request, 'accounts/register-page.html')
+
+
+def login_acc(request):
+    return render(request, 'accounts/login-page.html')
+
+
+def details_acc(request):
+    return render(request, 'accounts/profile-details-page.html')
+
+
+def edit_acc(request):
+    return render(request, 'accounts/profile-edit-page.html')
+
+
+def delete_acc(request):
+    return render(request, 'accounts/profile-delete-page.html')
