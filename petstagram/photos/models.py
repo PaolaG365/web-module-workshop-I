d9774477f6f8598ff29dfceb3aa606cadfad58dd
PaolaG365 <paola.georgieva.365@gmail.com>
2024-09-27 23:41:48 +0300

    photos model and pets model added

--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
+from petstagram.pets.models import Pet
+from petstagram.photos.validators import ImageSizeValidator
 
 # Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        upload_to='petPhotos/',
+        validators=[ImageSizeValidator(5, 'MB', 'The maximum image size can that be uploaded is 5Mb.')]
+    )
+
+    description = models.TextField(
+        max_length=300,
+        validators=[MinLengthValidator(10)],
+        null=True,
+        blank=True,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        to=Pet,
+        blank=True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now=True,
+    )
---
+++ petstagram/photos/validators.py
@@ -0,0 +1,43 @@
+from typing import Any
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+# i overdid it...
+file_size_map = {   
+    "B": 1,
+    "KB": 1024,
+    "MB": 1024 ** 2,
+    "GB": 1024 ** 3,
+    "TB": 1024 ** 4,
+}
+
+
+@deconstructible
+class ImageSizeValidator:
+    """Image size validator
+    Args:
+        limit: in integers, size limit for the file
+        data_size_type: unit of size (B, KB, MB, GB, TB)
+        message: custom error message if validation fails
+    """
+    def __init__(self, limit: int, data_size_type: str, message='Image size exceeds set limit'):
+        limit = limit
+        data_size_type = data_size_type
+        message= message
+
+    def __call__(self, file):
+        if file.size > self.limit * file_size_map[self.data_size_type]:
+            raise ValidationError(self.message)
+
+    def deconstruct(self):
+        return (
+            'petstagram.photos.validators.ImageSizeValidator',
+            (),
+            {
+                'message': self.message,
+                'limit': self.limit,
+                'data_size_type': self.data_size_type,
+                }
+        )
+
