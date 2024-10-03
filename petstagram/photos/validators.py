c0301693ed1e193cf1641ac3cf18853bfa165147
PaolaG365 <paola.georgieva.365@gmail.com>
2024-10-03 21:49:56 +0300

    minor change

--- petstagram/photos/validators.py
+++ petstagram/photos/validators.py
@@ -3,16 +3,6 @@ from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
 
-# i overdid it...
-file_size_map = {   
-    "B": 1,
-    "KB": 1024,
-    "MB": 1024 ** 2,
-    "GB": 1024 ** 3,
-    "TB": 1024 ** 4,
-}
-
-
 @deconstructible
 class ImageSizeValidator:
     """Image size validator
@@ -21,13 +11,21 @@ class ImageSizeValidator:
         data_size_type: unit of size (B, KB, MB, GB, TB)
         message: custom error message if validation fails
     """
+    FILE_SIZE_MAP = {   
+    "B": 1,
+    "KB": 1024,
+    "MB": 1024 ** 2,
+    "GB": 1024 ** 3,
+    "TB": 1024 ** 4,
+}
+
     def __init__(self, limit: int, data_size_type: str, message='Image size exceeds set limit'):
         self.limit = limit
         self.data_size_type = data_size_type
         self.message= message
 
     def __call__(self, file):
-        if file.size > self.limit * file_size_map[self.data_size_type]:
+        if file.size > self.limit * ImageSizeValidator.FILE_SIZE_MAP[self.data_size_type]:
             raise ValidationError(self.message)
 
     def deconstruct(self):
