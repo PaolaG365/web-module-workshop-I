6a51848333597f6b97a8e3858a544862de27bb3a
PaolaG365 <paola.georgieva.365@gmail.com>
2024-10-05 10:29:08 +0300

    added some functionality

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,7 +1,23 @@
+from lib2to3.fixes.fix_input import context
+from msilib.schema import ListView
+
 from django.shortcuts import render
+
+from petstagram.photos.models import Photo
+
 
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'common/home-page.html')
+    photos = Photo.objects.all()
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'common/home-page.html', context=context)
+
+
+# class HomePageView(ListView):
+#     model = Photo
+#     template_name = 'common/home-page.html'
+#     context_object_name = 'photos'
