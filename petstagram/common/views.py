4b529bf378be72b30b89e63feb186d83047ee13d
PaolaG365 <paola.georgieva.365@gmail.com>
2024-10-05 10:50:50 +0300

    like button func

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,8 +1,9 @@
 from lib2to3.fixes.fix_input import context
 from msilib.schema import ListView
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from petstagram.common.models import Like
 from petstagram.photos.models import Photo
 
 
@@ -21,3 +22,15 @@ def home_page(request):
 #     model = Photo
 #     template_name = 'common/home-page.html'
 #     context_object_name = 'photos'
+
+def like_functionality(request, photo_id):
+    photo = Photo.objects.get(pk=photo_id)
+    liked_obj = Like.objects.filter(to_photo_id=photo_id).first()
+
+    if liked_obj:
+        liked_obj.delete()
+    else:
+        like = Like(to_photo=photo)
+        like.save()
+
+    return redirect(request.META['HTTP_REFERER'] + f'#{photo_id}')
